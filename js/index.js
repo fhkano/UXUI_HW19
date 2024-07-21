9c28b9997c0b5f298f9a5394530b4e2a9f0b7f56
fhkano <kanofh16@gmail.com>
2024-07-21 10:21:39 -0700

    color change hover

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is linked");
+
+$("li").hover(function(){
+    $(this).css("background-color", "blue");
+    }, function(){
+    $(this).css("background-color", "white");
+
+});
